b160eb608272e0174d7a892d24a8b2bfa351b647
Quoc Pham <quoc_pham@msn.com>
2022-07-02 12:59:44 -0500

    Added new BinaryTree with insert and lookup

---
+++ HackerRank.Tests/BinarySearchTree2Tests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Tests
+{
+    public class BinarySearchTree2Tests
+    {
+        private ITree _tree;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tree = new BSTree();
+        }
+
+        [TestCase(new[] {9, 4, 6, 20, 170, 15, 1}, new[] {1, 2, 3, 2, 3, 3, 3})]
+        public void TestInsert(int[] input, int[] searchCounts)
+        {
+            for (var i = 0; i < input.Length; ++i)
+            {
+                var searchCount = _tree.Insert(input[i]);
+                Assert.AreEqual(searchCounts[i], searchCount);
+            }
+        }
+
+        [Test]
+        public void TestRandomLargeInsertCount()
+        {
+            var tree = new BSTree();
+            var rand = new Random();
+            var maxSearchCount = int.MinValue;
+            for (var i = 0; i < 100000; ++i)
+            {
+                var r = rand.Next(100000);
+                var searchCount = tree.Insert(r);
+                var expectedMax = Math.Max(3, Math.Log2(i) * 3);
+                Assert.That(searchCount, Is.LessThanOrEqualTo(expectedMax));
+                if (searchCount > maxSearchCount) maxSearchCount = searchCount;
+            }
+            Console.WriteLine($"Max Search Count: {maxSearchCount}");
+        }
+
+        [TestCase(new[] { 9, 4, 6, 20, 170, 15, 1, -12, 0, 7, 14, 19, 25, 35, 27, 205 }, new[] { 1, 2, 3, 2, 3, 3, 3 })]
+        public void TestLookup(int[] input, int[] searchCounts)
+        {
+            for (var i = 0; i < searchCounts.Length; ++i)
+            {
+                var searchCount = _tree.Insert(input[i]);
+                Assert.AreEqual(searchCounts[i], searchCount);
+            }
+
+            for (var i = 0; i < input.Length; ++i)
+            {
+                var searchCount = _tree.Lookup(input[i], out var found);
+                if (i < searchCounts.Length)
+                {
+                    Assert.AreEqual(searchCounts[i], searchCount);
+                    Assert.AreEqual(input[i], found.Value);
+                }
+                else
+                {
+                    Assert.IsNull(found);
+                }
+            }
+        }
+
+    }
+}
---
+++ HackerRank/BinarySearchTree2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public interface ITree
+    {
+        int Insert(int value);
+        int Lookup(int value, out BinaryNode foundNode);
+        bool Remove(int value);
+        int[] BreadthFirstSearch();
+    }
+
+    public class BinaryNode
+    { 
+        public BinaryNode Left;
+        public BinaryNode Right;
+        public int Value;
+    }
+
+    public class BSTree : ITree
+    {
+        #region Public
+        public int[] BreadthFirstSearch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(int value)
+        {
+            int searchCount = 1;
+            if (Root == null)
+            {
+                Root = new BinaryNode { Value = value };
+                return 1;
+            }
+
+            if (value < Root.Value) searchCount = Insert(Root, Root.Left, true, value, searchCount);
+            else searchCount = Insert(Root, Root.Right, false, value, 1);
+            return searchCount;
+        }
+
+        public int Lookup(int value, out BinaryNode foundNode)
+        {
+            var searchCount = 1;
+            if (Root == null) foundNode = null;
+            else if (value == Root.Value) foundNode = Root;
+            else
+            {
+                if (value < Root.Value) searchCount = Lookup(Root.Left, value, searchCount, out foundNode);
+                else searchCount = Lookup(Root.Right, value, searchCount, out foundNode);
+            }
+            return searchCount;
+        }
+
+        public bool Remove(int value)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Constructor
+        private BinaryNode Root;
+
+        public BSTree()
+        {
+            Root = null;
+        }
+        #endregion
+
+        #region Non-Public
+        private int Insert(BinaryNode parent, BinaryNode node, bool isLeft, int value, int searchCount)
+        {
+            searchCount++;
+            if (node == null)
+            {
+                node = new BinaryNode() { Value = value };
+                if (isLeft) parent.Left = node;
+                else parent.Right = node;
+                return searchCount;
+            }
+            if (value < node.Value) searchCount = Insert(node, node.Left, true, value, searchCount);
+            else searchCount = Insert(node, node.Right, false, value, searchCount);
+            return searchCount;
+        }
+
+        private int Lookup(BinaryNode node, int value, int searchCount, out BinaryNode foundNode)
+        {
+            searchCount++;
+            if (node == null) foundNode = null;
+            else if (node.Value == value) foundNode = node;
+            else
+            {
+                if (value < node.Value) searchCount = Lookup(node.Left, value, searchCount, out foundNode);
+                else searchCount = Lookup(node.Right, value, searchCount, out foundNode);
+            }
+            return searchCount;
+        }
+
+        #endregion
+    }
+}
