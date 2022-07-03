bad9dfe512a5abd323ff6ffe6a7f7272758977a8
Quoc Pham <quoc_pham@msn.com>
2022-07-03 16:05:04 -0500

    Add bottom up fibonacci

--- HackerRank/fibonacci.js
+++ HackerRank/fibonacci.js
@@ -6,7 +6,7 @@ function fibonacciRecursive(n) {
     return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 }
 
-function fibonacciMemoized() {
+function fibonacciMemoizedTopDown() {
     let cache = {};
     return function fib(n) {
         calculations++;
@@ -22,11 +22,27 @@ function fibonacciMemoized() {
     }
 }
 
+function fibonacciMemoizedBottomUp(n)
+{
+    let answers = [0, 1];
+    if (n < 2) return n;
+    for (let i = 2; i <= n; ++i)
+    {
+        calculations++;
+        answers.push(answers[i - 2] + answers[i - 1]);        
+    }
+    return answers.pop();
+}
+
 calculations = 0;
 console.log("Recursive", fibonacciRecursive(10));
 console.log("Recursive calcuations", calculations);
 
 calculations = 0;
-const memoizedFib = fibonacciMemoized();
-console.log('Memoized', memoizedFib(10));
+const memoizedFib = fibonacciMemoizedTopDown();
+console.log('Memoized Top Down', memoizedFib(10));
+console.log('Memoized Calculations', calculations);
+
+calculations = 0;
+console.log('Memoized Bottom Up', fibonacciMemoizedBottomUp(10));
 console.log('Memoized Calculations', calculations);
